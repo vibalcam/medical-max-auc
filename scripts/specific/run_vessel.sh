ba69995345e5a076f44b565b0fd4a4256b6e303b
Vicente <49537743+vibalcam@users.noreply.github.com>
2023-05-05 18:45:50 -0500

    Dropout best

--- scripts/specific/run_vessel.sh
+++ scripts/specific/run_vessel.sh
@@ -23,7 +23,29 @@ name=$d
 #     --use_best_model \
 #     --type_3d '3d'
 
-## best val auc
+# ## best val auc
+# python train.py \
+#     --name $name \
+#     --dataset $d \
+#     --save_dir $saved_folder \
+#     --workers $workers \
+#     --seed 123456 \
+#     --epochs 200 \
+#     --lr_steps 100 150 \
+#     --batch_size 32 \
+#     --lr 0.1 \
+#     --weight_decay 1e-5 \
+#     --epoch_decay 3e-3 \
+#     --margin 1.0 \
+#     --loss auc \
+#     --augmentations basic \
+#     --aug_args '' \
+#     --dropout 0 \
+#     --type_3d '3d' \
+#     --evaluate_every 5 \
+#     --early_stopping_patience 10
+
+## best val auc with dropout
 python train.py \
     --name $name \
     --dataset $d \
@@ -40,7 +62,7 @@ python train.py \
     --loss auc \
     --augmentations basic \
     --aug_args '' \
-    --dropout 0 \
+    --dropout 0.1 \
     --type_3d '3d' \
     --evaluate_every 5 \
     --early_stopping_patience 10
